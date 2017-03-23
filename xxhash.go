0c3aa314b221a64bec7eb9539116fb7bc12cbb18
Caleb Spare <cespare@gmail.com>
2017-03-23 16:26:12 -0700

    Write unsafe string->[]byte conversion to avoid liveness issue

--- xxhash.go
+++ xxhash.go
@@ -40,12 +40,13 @@ var (
 // TODO(caleb): We still have a function call; we could instead write Go/asm
 // copies of Sum64 for strings to squeeze out a bit more speed.
 func Sum64String(s string) uint64 {
+	// See https://groups.google.com/d/msg/golang-nuts/dcjzJy-bSpw/tcZYBzQqAQAJ
+	// for some discussion about this unsafe conversion.
 	var b []byte
-	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
 	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-	bh.Data = sh.Data
-	bh.Len = sh.Len
-	bh.Cap = sh.Len
+	bh.Data = (*reflect.StringHeader)(unsafe.Pointer(&s)).Data
+	bh.Len = len(s)
+	bh.Cap = len(s)
 	return Sum64(b)
 }
 
