b0b3ac4c8896b190538395d80e8389e20578efc4
Kaur Kuut <strom@nevermore.ee>
2017-04-06 07:37:38 +0300

    Separated unsafe code for App Engine support.

--- xxhash.go
+++ xxhash.go
@@ -5,8 +5,6 @@ package xxhash
 import (
 	"encoding/binary"
 	"hash"
-	"reflect"
-	"unsafe"
 )
 
 const (
@@ -30,25 +28,6 @@ var (
 	prime4v = prime4
 	prime5v = prime5
 )
-
-// Sum64String computes the 64-bit xxHash digest of s.
-// It may be faster than Sum64([]byte(s)) by avoiding a copy.
-//
-// TODO(caleb): Consider removing this if an optimization is ever added to make
-// it unnecessary: https://golang.org/issue//2205.
-//
-// TODO(caleb): We still have a function call; we could instead write Go/asm
-// copies of Sum64 for strings to squeeze out a bit more speed.
-func Sum64String(s string) uint64 {
-	// See https://groups.google.com/d/msg/golang-nuts/dcjzJy-bSpw/tcZYBzQqAQAJ
-	// for some discussion about this unsafe conversion.
-	var b []byte
-	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-	bh.Data = (*reflect.StringHeader)(unsafe.Pointer(&s)).Data
-	bh.Len = len(s)
-	bh.Cap = len(s)
-	return Sum64(b)
-}
 
 type xxh struct {
 	v1    uint64
