b5aede900e8eb16bf7e894108a437828bbaaffc6
Caleb Spare <cespare@gmail.com>
2022-12-01 11:06:45 -0800

    Simplify/tweak arm64 asm

--- xxhash_arm64.s
+++ xxhash_arm64.s
@@ -5,12 +5,12 @@
 
 #include "textflag.h"
 
-// Register allocation.
+// Register allocation:
 #define digest	R1
-#define h	R2	// Return value.
-#define p	R3	// Input pointer.
-#define len	R4
-#define nblocks	R5	// len / 32.
+#define h	R2	// return value
+#define p	R3	// input pointer
+#define n	R4      // input length
+#define nblocks	R5	// n / 32
 #define prime1	R7
 #define prime2	R8
 #define prime3	R9
@@ -36,19 +36,19 @@
 	ROR $64-31, x	\
 	MUL prime1, x	\
 
-#define mergeRound(x)			\
+#define mergeRound(acc, x)			\
 	round0(x)			\
-	EOR  x, h			\
-	MADD h, prime4, prime1, h	\
+	EOR  x, acc			\
+	MADD acc, prime4, prime1, acc	\
 
-// Update v[1-4] with 32-byte blocks. Assumes len >= 32.
+// Update v[1-4] with 32-byte blocks. Assumes n >= 32.
 #define blocksLoop()		\
-	LSR  $5, len, nblocks	\
+	LSR  $5, n, nblocks	\
 	PCALIGN $16		\
 loop:				\
-	LDP.P 32(p), (x1, x2)	\
+	LDP.P 16(p), (x1, x2)	\
+	LDP.P 16(p), (x3, x4)	\
 	round(v1, x1)		\
-	LDP  -16(p), (x3, x4)	\
 	round(v2, x2)		\
 	round(v3, x3)		\
 	round(v4, x4)		\
@@ -67,15 +67,15 @@ GLOBL primes<>(SB), NOPTR+RODATA, $40
 
 
 // func Sum64(b []byte) uint64
-TEXT ·Sum64(SB), NOFRAME+NOSPLIT, $0-32
-	LDP  b_base+0(FP), (p, len)
+TEXT ·Sum64(SB), NOSPLIT|NOFRAME, $0-32
+	LDP  b_base+0(FP), (p, n)
 
 	LDP  primes<> +0(SB), (prime1, prime2)
 	LDP  primes<>+16(SB), (prime3, prime4)
 	MOVD primes<>+32(SB), prime5
 
-	CMP  $32, len
-	CSEL LT, prime5, ZR, h // if len < 32 { h = prime5 } else { h = 0 }
+	CMP  $32, n
+	CSEL LT, prime5, ZR, h // if n < 32 { h = prime5 } else { h = 0 }
 	BLT  afterLoop
 
 	ADD  prime1, prime2, v1
@@ -93,68 +93,68 @@ TEXT ·Sum64(SB), NOFRAME+NOSPLIT, $0-32
 	ADD x3, x4
 	ADD x2, x4, h
 
-	mergeRound(v1)
-	mergeRound(v2)
-	mergeRound(v3)
-	mergeRound(v4)
+	mergeRound(h, v1)
+	mergeRound(h, v2)
+	mergeRound(h, v3)
+	mergeRound(h, v4)
 
 afterLoop:
-	ADD len, h
+	ADD n, h
 
-	TBZ   $4, len, try8
+	TBZ   $4, n, try8
 	LDP.P 16(p), (x1, x2)
 
 	round0(x1)
+	EOR  x1, h
 	ROR  $64-27, h
-	EOR  x1 @> 64-27, h, h
 	MADD h, prime4, prime1, h
 
 	round0(x2)
+	EOR  x2, h
 	ROR  $64-27, h
-	EOR  x2 @> 64-27, h
 	MADD h, prime4, prime1, h
 
 try8:
-	TBZ    $3, len, try4
+	TBZ    $3, n, try4
 	MOVD.P 8(p), x1
 
 	round0(x1)
+	EOR  x1, h
 	ROR  $64-27, h
-	EOR  x1 @> 64-27, h
 	MADD h, prime4, prime1, h
 
 try4:
-	TBZ     $2, len, try2
+	TBZ     $2, n, try2
 	MOVWU.P 4(p), x2
 
 	MUL  prime1, x2
+	EOR  x2, h
 	ROR  $64-23, h
-	EOR  x2 @> 64-23, h
 	MADD h, prime3, prime2, h
 
 try2:
-	TBZ     $1, len, try1
+	TBZ     $1, n, try1
 	MOVHU.P 2(p), x3
 	AND     $255, x3, x1
 	LSR     $8, x3, x2
 
 	MUL prime5, x1
+	EOR x1, h
 	ROR $64-11, h
-	EOR x1 @> 64-11, h
 	MUL prime1, h
 
 	MUL prime5, x2
+	EOR x2, h
 	ROR $64-11, h
-	EOR x2 @> 64-11, h
 	MUL prime1, h
 
 try1:
-	TBZ   $0, len, end
+	TBZ   $0, n, end
 	MOVBU (p), x4
 
 	MUL prime5, x4
+	EOR x4, h
 	ROR $64-11, h
-	EOR x4 @> 64-11, h
 	MUL prime1, h
 
 end:
@@ -171,7 +171,7 @@ end:
 // func writeBlocks(d *Digest, b []byte) int
 //
 // Assumes len(b) >= 32.
-TEXT ·writeBlocks(SB), NOFRAME+NOSPLIT, $0-40
+TEXT ·writeBlocks(SB), NOSPLIT|NOFRAME, $0-40
 	LDP  primes<>(SB), (prime1, prime2)
 
 	// Load state. Assume v[1-4] are stored contiguously.
@@ -179,7 +179,7 @@ TEXT ·writeBlocks(SB), NOFRAME+NOSPLIT, $0-40
 	LDP   0(digest), (v1, v2)
 	LDP  16(digest), (v3, v4)
 
-	LDP b_base+8(FP), (p, len)
+	LDP b_base+8(FP), (p, n)
 
 	blocksLoop()
 
@@ -187,6 +187,6 @@ TEXT ·writeBlocks(SB), NOFRAME+NOSPLIT, $0-40
 	STP (v1, v2),  0(digest)
 	STP (v3, v4), 16(digest)
 
-	BIC  $31, len
-	MOVD len, ret+32(FP)
+	BIC  $31, n
+	MOVD n, ret+32(FP)
 	RET
