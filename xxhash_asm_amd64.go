24dee9091bbfa992879dacb3cb38851d7d698c92
Jorropo <jorropo.pgm@gmail.com>
2024-06-18 21:27:59 +0200

    tweak CPUID so AVX512 impl only runs on amd

--- xxhash_asm_amd64.go
+++ xxhash_asm_amd64.go
@@ -7,7 +7,20 @@ package xxhash
 
 import "github.com/klauspost/cpuid/v2"
 
-var useAvx512 = cpuid.CPU.Supports(cpuid.AVX, cpuid.AVX2, cpuid.AVX512DQ, cpuid.AVX512F, cpuid.AVX512VL, cpuid.BMI1)
+var useAvx512 = cpuid.CPU.Supports(
+	cpuid.AVX,
+	cpuid.AVX2,
+	cpuid.AVX512DQ,
+	cpuid.AVX512F,
+	cpuid.AVX512VL,
+	cpuid.BMI1,
+
+// Today, vectorized 64 bits integer multiples positively sucks on intel,
+// with ILP a single scalar unit is multiple times faster.
+// This means sometime we wont be using the AVX512 when under virtualization
+// because vendor will be the hypervisor, but in my experience that is rare.
+// Most virtualization setups defaults to reporting the vendorid of the host.
+) && cpuid.CPU.IsVendor(cpuid.AMD)
 
 // Sum64 computes the 64-bit xxHash digest of b with a zero seed.
 func Sum64(b []byte) uint64 {
