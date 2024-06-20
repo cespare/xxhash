712cdadbec4759ffa9e51aef8f9d2224bf046bc0
Jorropo <jorropo.pgm@gmail.com>
2024-06-20 15:19:47 +0200

    remove slide and handle jump in avx512
    This removes the extra overhead and is cycle for cycle tied with pre-avx512 decision on non avx512.
    The PCALIGN on avx512 improves the performance from 28.5GiB/s to 30GiB/s on 4K, and 25GiB/s to 28GiB/s on 10M.
    This also removes avo because it were running in my legs as I couldn't get it to jump to an other function and cespare emited reservation to using it.

--- xxhash_asm_amd64.go
+++ xxhash_asm_amd64.go
@@ -1,8 +1,6 @@
 //go:build amd64 && !appengine && gc && !purego
 // +build amd64,!appengine,gc,!purego
 
-//go:generate sh -c "cd gen && go run -tags purego . -out xxhash_avx512_amd64.s"
-
 package xxhash
 
 import "github.com/klauspost/cpuid/v2"
@@ -23,36 +21,11 @@ var useAvx512 = cpuid.CPU.Supports(
 ) && cpuid.CPU.IsVendor(cpuid.AMD)
 
 // Sum64 computes the 64-bit xxHash digest of b with a zero seed.
-func Sum64(b []byte) uint64 {
-	if slideLength != 0 {
-		// If we don't have the slide sum small blocks are much slower.
-		if len(b) <= slideLength {
-			return slide(b)
-		}
-		if useAvx512 {
-			return sum64Avx512(b)
-		}
-	}
-	return sum64Scalar(b)
-}
-
+//
 //go:noescape
-func sum64Scalar(b []byte) uint64
-
-//go:noescape
-func sum64Avx512(b []byte) uint64
+func Sum64(b []byte) uint64
 
 // extra is a first block before b, it may be nil then skip it.
-func writeBlocks(d *Digest, extra *[32]byte, b []byte) {
-	if useAvx512 {
-		writeBlocksAvx512(&d.s, extra, b)
-		return
-	}
-	writeBlocksScalar(d, nil, b)
-}
-
+//
 //go:noescape
-func writeBlocksAvx512(d *[4]uint64, extra *[32]byte, b []byte)
-
-//go:noescape
-func writeBlocksScalar(d *Digest, extra *[32]byte, b []byte)
+func writeBlocks(d *Digest, extra *[32]byte, b []byte)
